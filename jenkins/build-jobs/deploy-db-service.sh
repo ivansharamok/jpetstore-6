201577fcd41b7df8ab3c7ce2ddd043a0d55f8034
ivansharamok <isharamok@gmail.com>
2018-06-20 14:30:48 -0700

    defined db-compose

---
+++ jenkins/build-jobs/deploy-db-service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script created and tested for Jenkins job
+# Jenkins project is expected to be parametrized and have these parameters defined:
+#   DTR_URL, DB_ROOT_USER_SECRET, DB_ROOT_PASSWORD_SECRET, DB_USER_SECRET, DB_PASSWORD_SECRET, STACK_NAME
+
+docker stack deploy -c ${WORKSPACE}/docker/compose/db-compose.yml ${STACK_NAME}
