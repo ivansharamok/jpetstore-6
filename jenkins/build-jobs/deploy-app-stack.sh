0c445d19d6e672d8c27e2ad561e015edf6fcac15
ivansharamok <isharamok@gmail.com>
2018-06-20 17:59:37 -0700

    fixed pass env var

--- jenkins/build-jobs/deploy-app-stack.sh
+++ jenkins/build-jobs/deploy-app-stack.sh
@@ -2,5 +2,5 @@
 # script created and tested for Jenkins job
 # Jenkins project is expected to be parametrized and have these parameters defined:
 #   DTR_URL, DB_ROOT_USER_SECRET, DB_ROOT_PASSWORD_SECRET, DB_USER_SECRET, DB_PASSWORD_SECRET, STACK_NAME
-echo "deploying stack '$STACK_NAME'..."
-docker stack deploy -c ${WORKSPACE}/docker/compose/db-compose.yml ${STACK_NAME}
+echo "deploying web app with stack '$STACK_NAME'..."
+docker stack deploy -c ${WORKSPACE}/docker/compose/jps-compose.yml ${STACK_NAME}
--- jenkins/build-jobs/deploy-db-service.sh
+++ jenkins/build-jobs/deploy-db-service.sh
@@ -2,5 +2,5 @@
 # script created and tested for Jenkins job
 # Jenkins project is expected to be parametrized and have these parameters defined:
 #   DTR_URL, DB_ROOT_USER_SECRET, DB_ROOT_PASSWORD_SECRET, DB_USER_SECRET, DB_PASSWORD_SECRET, STACK_NAME
-echo "deploying stack '$STACK_NAME'..."
+echo "deploying db service with stack '$STACK_NAME'..."
 docker stack deploy -c ${WORKSPACE}/docker/compose/db-compose.yml ${STACK_NAME}
